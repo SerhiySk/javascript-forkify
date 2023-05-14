34e372e29ea43414e3141f572ce42ae3ce4359aa
SerhiySk <skarbek.serhiy@gmail.com>
2023-05-14 14:10:48 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '04ae759c-8fba-4670-96f4-20923d40c8ac';
+export const MODAL_CLOSE_SEC = 2.5;
